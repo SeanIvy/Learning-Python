42b20ed1b8d48cd288ba94f32f1114ec3c023680
Sean Ivy <seanivy@me.com>
2012-05-16 18:07:48 -0400

    This is the file with extra credit applied.

--- ex15.py
+++ ex15.py
@@ -2,9 +2,9 @@
 # Header
 # ---------------------------------
 
-# LPTHW - Exercise 15
-author = "Sean Ivy - 051612"
-title = "Exercise 15 - Prompting and Passing"
+# http://learnpythonthehardway.org/book/ex15.html
+# Sean Ivy - 051612
+# Exercise 15 - Prompting and Passing
 
 # ---------------------------------
 # Start Code
@@ -13,28 +13,37 @@ title = "Exercise 15 - Prompting and Passing"
 # My personal header
 # ---------------------------------
 
+# This sets a variable that produces a line of dashes
 seperation = "-" *50
+# This sets a variable that references the line of dashes and places a line break before and after it.
 line = "\n%s\n" % seperation
 
+# sys is a group of system specific modules, this line tells my program to load the 'argv' module from sys.
 from sys import argv
 
+# this line gets values from the argv array, when we start the script originally we set these values. (i.e.: 'python [script name] [file name]'
 script, filename = argv
 
-print seperation
+# This prints the line of dashes set up before and adds a line break before it.
+print "\n%s" % seperation
+# I set this up as a joke, won't be on future scripts.
 print "Output of script", script, "."
-print seperation
-print title
-print author
+# This prints the line of dashes set up before and adds a line break after it.
 print "%s\n" % seperation
 
 # Now the Exercise
 # ---------------------------------
 
+# This is setting up a variable, as far as I can tell the variable 'txt' is set to get the text from whatever filename we specify when starting the script.
 txt = open(filename)
 
+# This line let's the user know we're about to print to the screen the text of whatever file was specified in argv (when opening the script).
 print "Here's your file, %r:\n" % filename
+
+# This line, as far as I know, opens the txt file specified when running the script, and then outputs the text to screen. I'm not sure what arguments *can* be called in the parens, though I'm assuming it can output database info too.
 print txt.read()
 
+# This prints the line of dashes set up before and adds a line break after it.
 print "%s\n" % seperation
 
 print "I'll also ask you to type it again:"
@@ -44,6 +53,7 @@ txt_again = open(file_again)
 
 print "\n", txt_again.read()
 
+# This prints the line of dashes set up before
 print seperation
 
 # ---------------------------------
