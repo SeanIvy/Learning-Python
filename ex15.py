ddbbd87092cfd0ddb49c3c3d8c55597ef37a3109
Sean Ivy <seanivy@me.com>
2012-05-16 16:38:28 -0400

    hing the updated ex15 to the repository

--- ex15.py
+++ ex15.py
@@ -30,7 +30,21 @@ print "%s\n" % seperation
 # Now the Exercise
 # ---------------------------------
 
+txt = open(filename)
 
+print "Here's your file, %r:\n" % filename
+print txt.read()
+
+print "%s\n" % seperation
+
+print "I'll also ask you to type it again:"
+file_again = raw_input(">")
+
+txt_again = open(file_again)
+
+print "\n", txt_again.read()
+
+print seperation
 
 # ---------------------------------
 # End Code
